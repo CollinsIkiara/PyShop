8c88a18f74d2eb295bb5c51853f856185ba29c93
Collins Ikiara <120456288+CollinsIkiara@users.noreply.github.com>
2026-05-07 12:13:40 -0700

    Change DEBUG setting to False
    Set DEBUG to False for production environment.

--- pyshop/settings.py
+++ pyshop/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k0z*33&o+_u_sj0@z+&&i3r2dv)vpw*8vg+tsbb!-*^k@h*+)0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'py-shop-nine.vercel.app',
