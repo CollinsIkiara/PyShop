7350306723eaed001a5649669ee45f407d5ce20c
Collins Ikiara <120456288+CollinsIkiara@users.noreply.github.com>
2026-05-07 12:18:50 -0700

    Change DEBUG setting to use environment variable

--- pyshop/settings.py
+++ pyshop/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k0z*33&o+_u_sj0@z+&&i3r2dv)vpw*8vg+tsbb!-*^k@h*+)0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = [
     'py-shop-nine.vercel.app',
